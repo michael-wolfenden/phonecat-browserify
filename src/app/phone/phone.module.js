5bc1e217d19952ec3a4ae1a40d2a50119f671e4a
Michael Wolfenden <michael.wolfenden@gmail.com>
2015-02-09 10:10:05 +1000

    Removed checkmark filter registration

--- src/app/phone/phone.module.js
+++ src/app/phone/phone.module.js
@@ -5,5 +5,4 @@ require('angular');
 module.exports = angular
     .module('app.phone', [])
     .controller('Phone', require('./phone'))
-    .filter('checkmark', require('./../core/filters/checkmark'))
     .run(require('./phone.route'));
